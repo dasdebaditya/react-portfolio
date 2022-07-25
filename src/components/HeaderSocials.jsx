b8594ea3d0b40ac619706c30042335f4c5c3c3aa
Debaditya Das <64529244+dasdebaditya@users.noreply.github.com>
2022-07-25 14:24:06 +0530

    Update HeaderSocials.jsx

--- src/components/HeaderSocials.jsx
+++ src/components/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/debaditya-das-5240a01a6/" target="_blank"><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/debaditya-das-b62681246/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/dasdebaditya" target="_blank"><BsGithub/></a>
         <a href="https://www.facebook.com/DebadityaDas1999" target="_blank"><BsFacebook/></a>
     </div>
